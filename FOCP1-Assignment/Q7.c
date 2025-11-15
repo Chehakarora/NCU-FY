9910c87cd3d03a7b35a03ddddf88b81cae1f7724
Chehak <chehakarora08@gmail.com>
2025-11-15 23:31:01 +0530

    Create Q7.c

---
+++ FOCP1-Assignment/Q7.c
@@ -0,0 +1,43 @@
+//C program to print a binary pyramid pattern
+#include <stdio.h>
+
+int main() {
+
+    int i, j;
+
+    for(i=1;i<=5;i++) {
+
+        for(j=1;j<=i;j++) {
+            if(j%2==0)
+                printf("1");
+            else
+                printf("0");
+        }
+
+        if(i!=5) {
+
+            for(j=1;j<=(5-i);j++) {
+                printf("  ");
+            }
+
+            for(j=1;j<=i;j++) {
+                if(j%2==0)
+                    printf("1");
+                else
+                    printf("0");
+            }
+        }
+
+        else {
+            for(j=1;j<=i;j++) {
+                if(j%2==0)
+                    printf("1");
+                else
+                    printf("0");
+            }
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
