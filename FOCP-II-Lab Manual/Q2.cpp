be47a4f07f7fb209e599542129766e8ac9ae6c5f
Chehak <chehakarora08@gmail.com>
2026-01-21 11:01:12 +0530

    Update Q2.cpp

--- FOCP-II-Lab Manual/Q2.cpp
+++ FOCP-II-Lab Manual/Q2.cpp
@@ -5,9 +5,9 @@ int main(){
     float radius;
     float area;
     float pi=3.14;
-    std::cout<<"Enter the radius of the fountain =";
+    std::cout<<"Enter the radius of the fountain = ";
     std::cin>>radius;
     area= pi*radius*radius;
-    std::cout<<"Area of the fountain ="<<area;
+    std::cout<<"Area of the fountain = "<<area;
     return 0;
 }
